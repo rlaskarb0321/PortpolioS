ad43e103864a3c568090b4407dbc6d36d6981e82
Kkabydol <rlaskarb0321@gmail.com>
2026-07-21 17:19:33 +0900

    [player input] resolve kcc asmdef problem

--- Assets/02. Scripts/Player/PlayerController.cs
+++ Assets/02. Scripts/Player/PlayerController.cs
@@ -1,9 +1,18 @@
+using System;
 using Fusion;
+using Fusion.Addons.KCC;
 using UnityEngine;
 
 public class PlayerController : NetworkBehaviour
 {
+    private KCC kcc;
+    
     public override void FixedUpdateNetwork()
+    {
+        
+    }
+
+    private void Awake()
     {
         
     }
