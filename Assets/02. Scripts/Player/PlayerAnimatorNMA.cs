df0c6b2b0dc15abbaa14033127da373d8d2eab31
Kkabydol <rlaskarb0321@gmail.com>
2026-07-23 17:00:14 +0900

    [player input] synchronize player walk animation

---
+++ Assets/02. Scripts/Player/PlayerAnimatorNMA.cs
@@ -0,0 +1,34 @@
+using System;
+using Fusion;
+using Fusion.Addons.KCC;
+using UnityEngine;
+
+public class PlayerAnimatorNMA : NetworkBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private float dampTime = 0.1f;
+    [SerializeField] private float maxSpeed = 5.0f;
+    
+    private NetworkMecanimAnimator networkMecanimAnimator;
+    private KCC kcc;
+    
+    private readonly int hashSpeed = Animator.StringToHash("Speed");
+
+    private void Awake()
+    {
+        kcc = GetComponent<KCC>();
+        networkMecanimAnimator = GetComponent<NetworkMecanimAnimator>();
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (HasStateAuthority == false && HasInputAuthority == false)
+            return;
+
+        var horizontal = kcc.Data.RealVelocity;
+        horizontal.y = 0.0f;
+
+        float speed = Mathf.Clamp01(horizontal.magnitude / maxSpeed);
+        animator.SetFloat(hashSpeed, speed, dampTime, Runner.DeltaTime);
+    }
+}
